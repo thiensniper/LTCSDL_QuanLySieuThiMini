97ed343a3e70a600f785c9ad755b42ba52ade283
Khoi <khoi.nguyen.tdmu@gmail.com>
2018-10-26 10:09:07 +0700

    Quản lý tài khoản Admin

--- QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmManageAccountAdmin.Designer.cs
+++ QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmManageAccountAdmin.Designer.cs
@@ -28,45 +28,280 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DevExpress.XtraGrid.GridLevelNode gridLevelNode3 = new DevExpress.XtraGrid.GridLevelNode();
+            DevExpress.XtraGrid.GridLevelNode gridLevelNode4 = new DevExpress.XtraGrid.GridLevelNode();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmManageAccountAdmin));
+            this.dgvAdminAccount = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnInsert = new DevExpress.XtraEditors.SimpleButton();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.cboAuthorization = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.txtPhone = new DevExpress.XtraEditors.TextEdit();
+            this.txtPassword = new DevExpress.XtraEditors.TextEdit();
+            this.txtUsername = new DevExpress.XtraEditors.TextEdit();
+            this.txtName = new DevExpress.XtraEditors.TextEdit();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupControl2 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdminAccount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cboAuthorization.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPhone.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).BeginInit();
+            this.groupControl2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dgvAdminAccount
+            // 
+            this.dgvAdminAccount.Dock = System.Windows.Forms.DockStyle.Fill;
+            gridLevelNode3.RelationName = "Level1";
+            gridLevelNode4.RelationName = "Level2";
+            this.dgvAdminAccount.LevelTree.Nodes.AddRange(new DevExpress.XtraGrid.GridLevelNode[] {
+            gridLevelNode3,
+            gridLevelNode4});
+            this.dgvAdminAccount.Location = new System.Drawing.Point(3, 17);
+            this.dgvAdminAccount.MainView = this.gridView1;
+            this.dgvAdminAccount.Name = "dgvAdminAccount";
+            this.dgvAdminAccount.Size = new System.Drawing.Size(663, 349);
+            this.dgvAdminAccount.TabIndex = 0;
+            this.dgvAdminAccount.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.dgvAdminAccount;
+            this.gridView1.Name = "gridView1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvAdminAccount);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(669, 369);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách tài khoản";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnInsert.ImageOptions.Image")));
+            this.btnInsert.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.btnInsert.Location = new System.Drawing.Point(8, 24);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(108, 62);
+            this.btnInsert.TabIndex = 0;
+            this.btnInsert.Text = "Thêm vào CSDL";
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.groupControl1.Controls.Add(this.cboAuthorization);
+            this.groupControl1.Controls.Add(this.txtPhone);
+            this.groupControl1.Controls.Add(this.txtPassword);
+            this.groupControl1.Controls.Add(this.txtUsername);
+            this.groupControl1.Controls.Add(this.txtName);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.label5);
+            this.groupControl1.Controls.Add(this.label3);
+            this.groupControl1.Controls.Add(this.label4);
+            this.groupControl1.Location = new System.Drawing.Point(675, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(365, 262);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "Thông tin tài khoản";
+            // 
+            // cboAuthorization
+            // 
+            this.cboAuthorization.Location = new System.Drawing.Point(120, 157);
+            this.cboAuthorization.Name = "cboAuthorization";
+            this.cboAuthorization.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cboAuthorization.Size = new System.Drawing.Size(236, 20);
+            this.cboAuthorization.TabIndex = 13;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(120, 125);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(236, 20);
+            this.txtPhone.TabIndex = 9;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.EditValue = "12345678";
+            this.txtPassword.Location = new System.Drawing.Point(120, 97);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(236, 20);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(120, 65);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(236, 20);
+            this.txtUsername.TabIndex = 11;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(120, 36);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(236, 20);
+            this.txtName.TabIndex = 12;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 56);
+            this.label1.Location = new System.Drawing.Point(5, 39);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Họ và tên:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(102, 66);
+            this.label2.Location = new System.Drawing.Point(5, 68);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tên đăng nhập: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Quyền: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mật khẩu: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "SĐT: ";
+            // 
+            // groupControl2
+            // 
+            this.groupControl2.Controls.Add(this.simpleButton2);
+            this.groupControl2.Controls.Add(this.simpleButton1);
+            this.groupControl2.Controls.Add(this.simpleButton3);
+            this.groupControl2.Controls.Add(this.btnInsert);
+            this.groupControl2.Location = new System.Drawing.Point(675, 268);
+            this.groupControl2.Name = "groupControl2";
+            this.groupControl2.Size = new System.Drawing.Size(365, 98);
+            this.groupControl2.TabIndex = 3;
+            this.groupControl2.Text = "Chức năng";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.simpleButton2.Location = new System.Drawing.Point(282, 24);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(74, 62);
+            this.simpleButton2.TabIndex = 0;
+            this.simpleButton2.Text = "Cập nhật";
+            this.simpleButton2.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.simpleButton1.Location = new System.Drawing.Point(202, 24);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(74, 62);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Xóa";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.simpleButton3.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.TopCenter;
+            this.simpleButton3.Location = new System.Drawing.Point(122, 24);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(74, 62);
+            this.simpleButton3.TabIndex = 0;
+            this.simpleButton3.Text = "Tạo mới";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
             // 
             // FrmManageAccountAdmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(593, 369);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1159, 406);
+            this.Controls.Add(this.groupControl2);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FrmManageAccountAdmin";
-            this.Text = "FrmManageAccountAdmin";
+            this.Text = "Quản lý tài khoản Admin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.FrmManageAccountAdmin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAdminAccount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.cboAuthorization.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPhone.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPassword.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtUsername.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl2)).EndInit();
+            this.groupControl2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private DevExpress.XtraGrid.GridControl dgvAdminAccount;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private DevExpress.XtraEditors.SimpleButton btnInsert;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.ComboBoxEdit cboAuthorization;
+        private DevExpress.XtraEditors.TextEdit txtPhone;
+        private DevExpress.XtraEditors.TextEdit txtPassword;
+        private DevExpress.XtraEditors.TextEdit txtUsername;
+        private DevExpress.XtraEditors.TextEdit txtName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private DevExpress.XtraEditors.GroupControl groupControl2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
     }
 }
--- QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmManageAccountAdmin.cs
+++ QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmManageAccountAdmin.cs
@@ -8,14 +8,74 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using System.Data.Linq;
 
 namespace QL_SIEU_THI_LTCSDL
 {
     public partial class FrmManageAccountAdmin : DevExpress.XtraEditors.XtraForm
     {
+        Table<tbl_TaiKhoan> tbl_AdminAccount;
+        DataAdminAccountDataContext db;
+        BindingManagerBase listAdminAccount;
+
         public FrmManageAccountAdmin()
         {
             InitializeComponent();
+        }
+
+        private void FrmManageAccountAdmin_Load(object sender, EventArgs e)
+        {
+            db = new DataAdminAccountDataContext();
+            tbl_AdminAccount = db.tbl_TaiKhoans;
+
+            LoadDataToCBOAuthorization();
+            LoadDatabaseToDataGridView();
+
+            //Binding
+            txtName.DataBindings.Add("Text", tbl_AdminAccount, "HoTen", true);
+            txtUsername.DataBindings.Add("Text", tbl_AdminAccount, "TenDangNhap", true);
+            txtPassword.DataBindings.Add("Text", tbl_AdminAccount, "MatKhau", true);
+            txtPhone.DataBindings.Add("Text", tbl_AdminAccount, "Sdt", true);
+            cboAuthorization.DataBindings.Add("Text", tbl_AdminAccount, "Quyen", true);
+            listAdminAccount = this.BindingContext[tbl_AdminAccount];
+        }
+
+        private void LoadDataToCBOAuthorization()
+        {
+            cboAuthorization.Properties.Items.Add("Manager");
+            cboAuthorization.Properties.Items.Add("Import");
+            cboAuthorization.Properties.Items.Add("Export");
+
+            cboAuthorization.SelectedIndex = 0;
+        }
+
+        private void LoadDatabaseToDataGridView()
+        {
+            dgvAdminAccount.DataSource = tbl_AdminAccount;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listAdminAccount.EndCurrentEdit();
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            listAdminAccount.AddNew();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            listAdminAccount.RemoveAt(listAdminAccount.Position);
+            db.SubmitChanges();
         }
     }
 }
