036ac747ae59de8fa3861bd4f3ec57886e0a3f16
Khoi <khoi.nguyen.tdmu@gmail.com>
2018-10-26 08:58:03 +0700

    Quản lý tài khoản Admin, Khách hàng

---
+++ QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmManageAccountCustomercs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace QL_SIEU_THI_LTCSDL
+{
+    public partial class FrmManageAccountCustomercs : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmManageAccountCustomercs()
+        {
+            InitializeComponent();
+        }
+    }
+}
