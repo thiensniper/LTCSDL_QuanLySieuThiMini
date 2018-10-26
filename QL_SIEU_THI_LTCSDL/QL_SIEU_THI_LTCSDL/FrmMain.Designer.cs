37b322576d05f28ce00d466a7eb4f9e9568e9b1f
Thiện Thần Thánh <nguyenminhthiensmart@gmail.com>
2018-10-26 07:47:37 +0700

    Update FrmMain

--- QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmMain.Designer.cs
+++ QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QL_SIEU_THI_LTCSDL
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
--- QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmMain.cs
+++ QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/FrmMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QL_SIEU_THI_LTCSDL
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        public FrmMain()
         {
             InitializeComponent();
         }
