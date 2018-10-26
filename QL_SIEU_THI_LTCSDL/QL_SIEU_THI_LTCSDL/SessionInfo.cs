c05e42ed63c94e0dbfdd95a5ae6009d3215e281c
Thiện Thần Thánh <nguyenminhthiensmart@gmail.com>
2018-10-26 10:21:18 +0700

    Add Session

---
+++ QL_SIEU_THI_LTCSDL/QL_SIEU_THI_LTCSDL/SessionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QL_SIEU_THI_LTCSDL
+{
+    public static class SessionInfo
+    {
+
+    }
+}
